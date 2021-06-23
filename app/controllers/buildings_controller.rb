db7d9cf23484dbcd76ab51eb4e5e0c63d55654c7
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-23 16:35:51 +0900

    コントローラの余計な改行やコメントを削除し、インデントを整えました

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,31 +1,20 @@
 class BuildingsController < ApplicationController
   before_action :set_building, only: %i[ show edit update destroy ]
-
-  # GET /buildings or /buildings.json
   def index
     @buildings = Building.all
   end
-
-  # GET /buildings/1 or /buildings/1.json
   def show
     @stations = @building.stations
   end
-
-  # GET /buildings/new
   def new
     @building = Building.new
     2.times { @building.stations.build }
   end
-
-  # GET /buildings/1/edit
   def edit
     @building.stations.build
   end
-
-  # POST /buildings or /buildings.json
   def create
     @building = Building.new(building_params)
-
     respond_to do |format|
       if @building.save
         format.html { redirect_to @building, notice: "Building was successfully created." }
@@ -36,8 +25,6 @@ class BuildingsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /buildings/1 or /buildings/1.json
   def update
     respond_to do |format|
       if @building.update(building_params)
@@ -49,8 +36,6 @@ class BuildingsController < ApplicationController
       end
     end
   end
-
-  # DELETE /buildings/1 or /buildings/1.json
   def destroy
     @building.destroy
     respond_to do |format|
@@ -58,20 +43,16 @@ class BuildingsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_building
-      @building = Building.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def building_params
-      params.require(:building).permit(
-        :name, :rent, :address, :age, :note,
-        stations_attributes: [
-          :id, :railway, :name, :walking_time, :building_id
-          ]
-        )
-    end
+  def set_building
+    @building = Building.find(params[:id])
+  end
+  def building_params
+    params.require(:building).permit(
+      :name, :rent, :address, :age, :note,
+      stations_attributes: [
+        :id, :railway, :name, :walking_time, :building_id
+      ]
+    )
+  end
 end
